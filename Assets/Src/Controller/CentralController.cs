cd0a6df40d09791eb218fecae32afb3b97aa99bb
eckolo <eckolo.0722@gmail.com>
2020-12-23 03:12:53 +0900

    初期化処理周りの概形仮組み

---
+++ Assets/Src/Controller/CentralController.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Src.Model.Application.Service;
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Src.Controller
+{
+    /// <summary>
+    /// 処理全体の中央制御系
+    /// </summary>
+    public class CentralController
+    {
+        /// <summary>
+        /// メインルーチン制御の実行
+        /// </summary>
+        /// <returns>イテレータ</returns>
+        public async UniTask ExecuteMainRoutine()
+        //TODO ビュー（ビューコントローラ）呼び出し処理実装
+        //TODO 戦闘処理の呼び出しとか普段のゲームルーチン設計
+        {
+            Debugger.Log($"{typeof(CentralController).FullName}.{nameof(ExecuteMainRoutine)}()");
+        }
+    }
+}
---
+++ Assets/Src/Controller/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Src.Model.Application.Service;
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Src.Controller
+{
+    /// <summary>
+    /// 初期化処理制御系
+    /// </summary>
+    public class StartUp : MonoBehaviour
+    {
+        /// <summary>
+        /// 最初期起動関数
+        /// とりあえず自己生成するだけ
+        /// </summary>
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void Initialize()
+        {
+            Debugger.Log($"{typeof(StartUp).FullName}.{nameof(Initialize)}()");
+            var myself = new GameObject(nameof(StartUp), typeof(StartUp));
+        }
+
+        /// <summary>
+        /// シーン読み込み後に呼ばれるコールバック
+        /// </summary>
+        async void Start()
+        {
+            Debugger.Log($"{typeof(StartUp).FullName}.{nameof(Start)}()");
+            await SetUp();
+        }
+
+        /// <summary>
+        /// システム的な初期処理
+        /// </summary>
+        /// <returns>初期処理正常完了フラグ</returns>
+        async UniTask<bool> SetUp()
+        {
+            Debugger.Log($"{typeof(StartUp).FullName}.{nameof(SetUp)}()");
+
+            try
+            {
+                var controller = new CentralController();
+                await controller.ExecuteMainRoutine();
+            }
+            catch(Exception error)
+            {
+                Debugger.LogException(error);
+                //デバッグ実行時は原因箇所追いやすくするために直接エラーを投げる
+                if(!Debug.isDebugBuild)
+                    throw error;
+            }
+            return true;
+        }
+    }
+}
