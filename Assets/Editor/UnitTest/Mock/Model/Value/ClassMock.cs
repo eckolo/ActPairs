f1bcacc2ca53bc7184f1885d2a8aa9b23b9a0866
eckolo <eckolo.0722@gmail.com>
2020-12-25 00:42:03 +0900

    数値計算サービス追加

---
+++ Assets/Editor/UnitTest/Mock/Model/Value/ClassMock.cs
@@ -0,0 +1,48 @@
+﻿using Assets.Src.Model.Application.Value;
+using System;
+using UnityEngine;
+
+namespace Assets.Editor.UnitTest.Mock.Model.Value
+{
+    /// <summary>
+    /// ただ値を持つだけのクラスのモック
+    /// </summary>
+    [Serializable]
+    public class ClassMock : IDuplicatable<ClassMock>, IComparable<ClassMock>
+    {
+        [SerializeField]
+        private string _text1 = "";
+        public string text1 { get => _text1; set => _text1 = value; }
+        [SerializeField]
+        private string _text2 = "";
+        public string text2 { get => _text2; set => _text2 = value; }
+        [SerializeField]
+        private int _number = 0;
+        public int number { get => _number; set => _number = value; }
+        [SerializeField]
+        private InnerClassMock _innerClass = new InnerClassMock();
+        public InnerClassMock innerClass { get => _innerClass; set => _innerClass = value; }
+
+        [Serializable]
+        public class InnerClassMock
+        {
+            [SerializeField]
+            private string _text1 = "";
+            public string text1 { get => _text1; set => _text1 = value; }
+            [SerializeField]
+            private string _text2 = "";
+            public string text2 { get => _text2; set => _text2 = value; }
+            [SerializeField]
+            private int _number = 0;
+            public int number { get => _number; set => _number = value; }
+        }
+
+        /// <summary>
+        /// シャローコピーメソッド
+        /// </summary>
+        /// <returns>コピーされたオブジェクト</returns>
+        public ClassMock MemberwiseClonePublic() => (ClassMock)MemberwiseClone();
+
+        public int CompareTo(ClassMock other) => number.CompareTo(other.number);
+    }
+}
