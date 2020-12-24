75d12310a028efd27468d34594dd0735ac809f70
eckolo <eckolo.0722@gmail.com>
2020-12-25 00:25:07 +0900

    複製可能オブジェクトのインターフェース定義追加

---
+++ Assets/Src/Model/Application/Value/IDuplicatable.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Assets.Src.Model.Application.Value
+{
+    /// <summary>
+    /// シャローコピー可能オブジェクトインターフェース
+    /// </summary>
+    /// <typeparam name="Duplicated">コピー対象オブジェクトクラス</typeparam>
+    public interface IDuplicatable<Duplicated> where Duplicated : IDuplicatable<Duplicated>
+    {
+        /// <summary>
+        /// シャローコピーメソッド
+        /// 基本的にMemberwiseClone()メソッドをキャストだけしてそのまま返せばOK
+        /// </summary>
+        /// <returns>コピーされたオブジェクト</returns>
+        Duplicated MemberwiseClonePublic();
+    }
+}
