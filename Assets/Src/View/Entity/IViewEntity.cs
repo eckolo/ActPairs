15b047af7cac1e668d8a4bda752deeafa921a519
eckolo <eckolo.0722@gmail.com>
2021-01-03 22:48:07 +0900

    ビュー状態操作用途の情報受け渡し用データクラス定義

---
+++ Assets/Src/View/Entity/IViewEntity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Src.View.Entity
+{
+    /// <summary>
+    /// ビュー実体のパラメータ定義インターフェース
+    /// </summary>
+    public interface IViewEntity
+    {
+        /// <summary>
+        /// 座標パラメータ
+        /// </summary>
+        ViewStateParameter<Vector2> coordinate { get; }
+        /// <summary>
+        /// 透明度パラメータ
+        /// </summary>
+        ViewStateParameter<float> alpha { get; }
+        /// <summary>
+        /// サイズ倍率パラメータ
+        /// </summary>
+        ViewStateParameter<Vector2> size { get; }
+        /// <summary>
+        /// 層番号パラメータ
+        /// </summary>
+        ViewStateParameter<int> layer { get; }
+    }
+}
---
+++ Assets/Src/View/Entity/ViewStateParameter.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Src.Model.Application.Value;
+using System.Collections.Generic;
+using System.Linq;
+
+#nullable enable
+namespace Assets.Src.View.Entity
+{
+    /// <summary>
+    /// ビューパラメータ計算用の諸々の値を持つデータクラス
+    /// </summary>
+    /// <typeparam name="TValue">パラメータ型</typeparam>
+    public class ViewStateParameter<TValue> where TValue : struct
+    {
+        /// <summary>
+        /// 基点値
+        /// </summary>
+        public TValue basis { get; set; }
+        /// <summary>
+        /// 変動値パラメータ
+        /// </summary>
+        public IEnumerable<(Easing easing, int time, TValue final)> progresses { get; set; }
+            = Enumerable.Empty<(Easing, int, TValue)>();
+    }
+}
